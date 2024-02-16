eacfb1093ef7f5fa27eda128a62432a75448fcd1
kalyan237 <144648493+kalyan237@users.noreply.github.com>
2024-02-16 09:11:17 +0530

    region and instance change

--- ec2.tf
+++ ec2.tf
@@ -11,13 +11,13 @@ terraform {
 # Provider Block
 provider "azure" {
   profile = "default" 
-  region  = "us-east-1"
+  region  = "ap-south"
 }
 
 #Resource Block
 resource "azure_instance" "ec2testserver" {
   ami           = "ami-041db4a969fe3eb68" 
-  instance_type = "t2.micro"
+  instance_type = "m1.large"
 
 }
 
